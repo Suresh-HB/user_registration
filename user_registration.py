aa5a129339ef148235da5c4607c68417a6df8df0
Suresh HB <sureshsai175475@gmail.com>
2024-08-09 16:57:38 +0530

    [Suresh] Add: As a user need to enter a valid First Name and Last Name

--- user_registration.py
+++ user_registration.py
@@ -1,10 +1,10 @@
 '''
-
 @Author: Suresh
-@Date: 2024-08-08
+@Date: 2024-08-09
 @Last Modified by: Suresh
-@Last Modified: 2024-08-08 
-@Title : As a User need to enter a valid First Name
+@Last Modified: 2024-08-09 
+@Title : As a user need to enter a valid First Name and Last Name 
+
 '''
 
 import re
@@ -16,18 +16,36 @@ def valid_frist_name(frist_name):
     """
     Description: Function to validating user frist name as per the pattern. 
     Parameters:
-        frist_name : Function taking the user name as a parameter for match the pattern.
+        frist_name : User frist name to be validated.
     Returns:
-        res : returning the bool value to main function 
+        res : True if the frist name matches the pattern, False otherwise. 
     """
 
     logger.info(" inside valid_frist_name function")
 
     frist_name_pattern = r'[A-Z][a-z]{2,}'
     res = bool(re.match(frist_name_pattern, frist_name))
-    logger.debug(f" validating the user frist name according to pattern matching = '{frist_name_pattern}'")
+    logger.debug(f" validating the user frist name according to pattern matching ")
     logger.info(" returning the bool value to main function")
     return res
+
+
+def valid_last_name(last_name):
+
+    """
+    Description: Function to validate the user's last name according to the pattern.
+    Parameters:
+        last_name : User last name to be validated.
+    Returns:
+        result : True if the last name matches the pattern, False otherwise.
+    """
+
+    logger.info(" Started valid_last_name function")
+    last_name_pattern = r'[A-z][a-z]{2,}'
+    result = bool(re.match(last_name_pattern,last_name))
+    logger.debug(f" validating the user last name according to pattern matching")
+    logger.info(" returning the bool value to main function")
+    return result
 
 
 def main():
@@ -35,14 +53,25 @@ def main():
 
     frist_name = input(" Enter your frist name : ")
     logger.debug(f" Taking frist name as input from the user : {frist_name}")
-    
-    logger.info(" calling valid_frist_name function")
+
+    last_name = input("Enter last name of the user : ")
+    logger.debug(f" Taking last name as input from the user : {last_name}")
+
+    logger.info(" calling valid_frist_name method")
     res = valid_frist_name(frist_name)
 
     if res == True:
-        logger.info(f" User entered frist name = '{frist_name}' is valid")
+        
+        logger.info(" calling valid_last_name method")
+        result = valid_last_name(last_name)
+        if result:
+            logger.info(f" User Entered Frist name = '{frist_name}' is valid")
+            logger.info(f" User Entered Last name = '{last_name}' is valid")
+        else:
+            logger.info(f" User Entered Last name = '{last_name}' is invalid")
+
     else:
-        logger.info(f" User entered frist name = '{frist_name}' invalid")
+        logger.info(f" User entered Frist name = '{frist_name}' invalid")
 
     logger.info(" exit main function")
 
